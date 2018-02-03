d30ee0b8aa3dc57727067fd22b75aeb1de51c122
Andrew Miller <milldawg@github.com>
2018-02-03 01:25:02 +0000

    Added PickerView functionality, without crash, and before PickerBtn change to County

--- MiraclePill/ViewController.swift
+++ MiraclePill/ViewController.swift
@@ -8,18 +8,40 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UIPickerViewDataSource, UIPickerViewDelegate {
+    
+    @IBOutlet weak var countyPicker: UIPickerView!
+    @IBOutlet weak var countyPickerBtn: UIButton!
+    
+    let counties = ["Bedfordshire","Berkshire","Buckinghamshire","Cambridgeshire","Cheshire","Cornwall","Cumberland","Derbyshire","Devon","Dorset","Durham","Essex","Gloucestershire","Hampshire","Herefordshire","Hertfordshire","Huntingdonshire","Kent","Lancashire","Leicestershire","Lincolnshire","Middlesex","Norfolk","Northamptonshire","Northumberland","Nottinghamshire","Oxfordshire","Rutland","Shropshire","Somerset","Staffordshire","Suffolk","Surrey","Sussex","Warwickshire","Westmorland","Wiltshire","Worcestershire","Yorkshire"]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        countyPicker.dataSource = self
+        countyPicker.delegate = self
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    
+    @IBAction func countyBtnPressed(_ sender: Any) {
+    }
+    
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return counties.count
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        return counties[row]
+    }
+    
 }
 
