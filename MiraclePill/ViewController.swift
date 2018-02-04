c2d1632c9ea4ed8a0779c61f8510c8d1ff91c45f
Andrew Miller <milldawg@github.com>
2018-02-04 18:53:03 +0000

    Extending MiraclePill Take 2 - added LaunchScreen, sorted button usage, and hiding of fields

--- MiraclePill/ViewController.swift
+++ MiraclePill/ViewController.swift
@@ -12,14 +12,28 @@ class ViewController: UIViewController, UIPickerViewDataSource, UIPickerViewDele
     
     @IBOutlet weak var countyPicker: UIPickerView!
     @IBOutlet weak var countyPickerBtn: UIButton!
+    @IBOutlet weak var countryTitle: UILabel!
+    @IBOutlet weak var countryText: UITextField!
+    @IBOutlet weak var postcodeTitle: UILabel!
+    @IBOutlet weak var postcodeText: UITextField!
+    @IBOutlet weak var buyNowBtn: UIButton!
+    @IBOutlet weak var cityText: UITextField!
+    @IBOutlet weak var streetAddressText: UITextField!
+    @IBOutlet weak var fullNameText: UITextField!
     
     let counties = ["Bedfordshire","Berkshire","Buckinghamshire","Cambridgeshire","Cheshire","Cornwall","Cumberland","Derbyshire","Devon","Dorset","Durham","Essex","Gloucestershire","Hampshire","Herefordshire","Hertfordshire","Huntingdonshire","Kent","Lancashire","Leicestershire","Lincolnshire","Middlesex","Norfolk","Northamptonshire","Northumberland","Nottinghamshire","Oxfordshire","Rutland","Shropshire","Somerset","Staffordshire","Suffolk","Surrey","Sussex","Warwickshire","Westmorland","Wiltshire","Worcestershire","Yorkshire"]
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        buyNowBtn.isEnabled = false
         countyPicker.dataSource = self
         countyPicker.delegate = self
+        fullNameText.addTarget(self, action: #selector(editingChanged), for: .editingChanged)
+        streetAddressText.addTarget(self, action: #selector(editingChanged), for: .editingChanged)
+        cityText.addTarget(self, action: #selector(editingChanged), for: .editingChanged)
+        countryText.addTarget(self, action: #selector(editingChanged), for: .editingChanged)
+        postcodeText.addTarget(self, action: #selector(editingChanged), for: .editingChanged)
     }
     
     override func didReceiveMemoryWarning() {
@@ -30,6 +44,10 @@ class ViewController: UIViewController, UIPickerViewDataSource, UIPickerViewDele
     
     @IBAction func countyBtnPressed(_ sender: Any) {
         countyPicker.isHidden = false
+        countryText.isHidden = true
+        countryTitle.isHidden = true
+        postcodeText.isHidden = true
+        postcodeTitle.isHidden = true
     }
     
     func numberOfComponents(in pickerView: UIPickerView) -> Int {
@@ -47,6 +65,30 @@ class ViewController: UIViewController, UIPickerViewDataSource, UIPickerViewDele
     func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
         countyPickerBtn.setTitle(counties[row], for: UIControlState.normal)
         countyPicker.isHidden = true
+        countryText.isHidden = false
+        countryTitle.isHidden = false
+        postcodeText.isHidden = false
+        postcodeTitle.isHidden = false
+    }
+    
+    @objc func editingChanged(_ textField: UITextField) {
+        if textField.text?.count == 1 {
+            if textField.text?.first == " " {
+                textField.text = ""
+                return
+            }
+        }
+        guard
+            let fullName = fullNameText.text, !fullName.isEmpty,
+            let streetAddress = streetAddressText.text, !streetAddress.isEmpty,
+            let city = cityText.text, !city.isEmpty,
+            let country = countryText.text, !country.isEmpty,
+            let postcode = postcodeText.text, !postcode.isEmpty
+            else {
+                buyNowBtn.isEnabled = false
+                return
+        }
+        buyNowBtn.isEnabled = true
     }
     
 }
